d577e2d154bbbdc2c4dd9d95b3fe08ddad0156ab
Wynstona Jackreece <wjackreece@gmail.com>
2023-08-15 22:20:10 -0400

    Created frame for Day001

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Day001 from "./components/Day001/Day001";
 
 function App() {
-  return <div>Hello Tonye</div>;
+  return (
+    <div>
+      <Day001 />
+    </div>
+  );
 }
 
 export default App;
