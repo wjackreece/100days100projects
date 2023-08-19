a54fbf0f73b4bd45785831aae2ab791c00f52a5a
Wynstona Jackreece <wjackreece@gmail.com>
2023-08-18 23:13:13 -0400

    Removed Controller.js and modified App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import DataFetching from "./components/DataFetching/DataFetching";
-// import Day001 from "./components/Day001/Day001";
 
 function App() {
   return (
     <div>
-      {/* <Day001 /> */}
       <DataFetching />
     </div>
   );
