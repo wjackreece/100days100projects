68ca2c9955f73771f76f66ee95a66bd73e8414dc
Wynstona Jackreece <wjackreece@gmail.com>
2023-08-25 21:02:01 -0400

    Installed react-icons

--- src/components/Day002/Day002.js
+++ src/components/Day002/Day002.js
@@ -1,8 +1,40 @@
 import React from "react";
+import {
+  TbMoodSmile,
+  TbMoodEmpty,
+  TbMoodHappy,
+  TbMoodSad,
+  TbMoodNeutral,
+} from "react-icons/tb";
+
 import "./Day002.css";
 
 function Day002() {
-  return <div>Day002</div>;
+  return (
+    <div>
+      <h1> 2023 Mood Calendar</h1>
+      <h2>~ color each day depending on what your mood was / is ~</h2>
+
+      <h3>Select mood:</h3>
+      <div>
+        <button>
+          <TbMoodHappy />
+        </button>
+        <button>
+          <TbMoodSmile />
+        </button>
+        <button>
+          <TbMoodEmpty />
+        </button>{" "}
+        <button>
+          <TbMoodSad />
+        </button>
+        <button>
+          <TbMoodNeutral />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Day002;
