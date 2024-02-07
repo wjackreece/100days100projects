913d1409866487feee4fe640d3e7931f23048b56
Wynstona Jackreece <wjackreece@gmail.com>
2024-02-07 14:12:14 -0500

    Installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DataFetching from "./components/DataFetching/DataFetching";
 import Day002 from "./components/Day002/Day002";
 import ProjectList from "./components/ProjectList/ProjectList";
